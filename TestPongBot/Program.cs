3b6333e00dede664948c21fd20648ceb59b52f18
xianthe <xianthe.xian@gmail.com>
2017-05-18 17:59:36 -0700

    Initial commit

---
+++ TestPongBot/Program.cs
@@ -0,0 +1,42 @@
+﻿using Discord;
+using Discord.WebSocket;
+using System;
+using System.Threading.Tasks;
+
+namespace TestPongBot
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+            => new Program().MainAsync().GetAwaiter().GetResult();
+
+        public async Task MainAsync()
+        {
+            var client = new DiscordSocketClient();
+
+            client.Log += Log;
+            client.MessageReceived += MessageReceived;
+
+            string token = "MzEzODkwNTczNDA2ODMwNTky.C_5evQ.-6NxiFxs9ZHCrNvtgNl5fEd1eUg"; // Remember to keep this private!
+            await client.LoginAsync(TokenType.Bot, token);
+            await client.StartAsync();
+
+            // Block this task until the program is closed.
+            await Task.Delay(-1);
+        }
+
+        private async Task MessageReceived(SocketMessage message)
+        {
+            if (message.Content == "!ping")
+            {
+                await message.Channel.SendMessageAsync("Pong!");
+            }
+        }
+
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+        }
+    }
+}
